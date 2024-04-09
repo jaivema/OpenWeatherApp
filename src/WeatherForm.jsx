717c846fd3d92a6259b284dfe2ccec3ee60956bf
jaivema <jaivema@hotmail.com>
2024-04-09 12:29:11 +0200

    dos botones de busqueda

--- src/WeatherForm.jsx
+++ src/WeatherForm.jsx
@@ -77,8 +77,19 @@ function WeatherForm( {setWeather}) {
                 loading={loading}
                 loadingIndicator="Buscando..."
                 endIcon={<SendIcon/>}
+                
             >
-                Buscar
+                Ciudad
+            </LoadingButton>
+            <LoadingButton 
+                type="submit" 
+                variant="contained"
+                loading={loading}
+                loadingIndicator="Buscando..."
+                endIcon={<SendIcon/>}
+                
+            >
+                Pais
             </LoadingButton>
         </Box>
     );
